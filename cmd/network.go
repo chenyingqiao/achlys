149aa22bb52732333095f879e9c90a17f02bdebb
chenyj03@mingyuanyun.com <chenyingqiao19931115@gmail.com>
2023-09-26 07:45:02 +0800

    feat: 添加网络实现

---
+++ cmd/network.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"net"
+
+	"github.com/vishvananda/netlink"
+)
+
+type Network struct{
+	Name string
+	IpRange *net.IPNet
+	Driver string
+}
+
+type Endpoint struct{
+	ID string
+	Device netlink.Veth
+	IPAddress net.IP
+	MacAddress net.HardwareAddr
+	PortMapping []string
+	Network *Network
+}
+
+type Driver interface{
+	Name() string
+	Create(subnet string, name string) (*Network, error)
+	Delete(network Network)
+	Connect(network *Network,endpoint *Endpoint)
+	Disconnect(network Network, endpoint *Endpoint)
+}
+
+func CreateNetwork(driver, subnet, name string) error {
+	_, cidr, _ := net.ParseCIDR(subnet)
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module mydocker
 go 1.19
 
 require (
+	github.com/spf13/cobra v1.7.0
+	github.com/vishvananda/netlink v1.1.0
+)
+
+require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.7.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/vishvananda/netns v0.0.0-20191106174202-0a2b9b5464df // indirect
+	golang.org/x/sys v0.11.0 // indirect
 )
